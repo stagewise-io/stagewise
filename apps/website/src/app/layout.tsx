f386b7d0a88628aaa017dee594d84b359df5fc67
xn1cklas <nicklas.scharpff@nsquare.one>
2025-04-30 10:24:38 +0100

    feat(website): implement Navbar component and integrate it into the layout

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import '@stagewise/ui/globals.css';
+import { Navbar } from '@/components/nav-bar';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,6 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="fixed top-0 right-0 left-0 z-50">
+          <nav className="container mx-auto">
+            <Navbar />
+          </nav>
+        </header>
         {children}
       </body>
     </html>
