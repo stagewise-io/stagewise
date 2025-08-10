0e4ea34a079899cae1788706f3ae8b96b0070ffb
Glenn Töws <glenntoews@outlook.com>
2025-08-10 11:12:06 -0700

    fix(karton-contract): add toolcall approvals into state and export messgae part types

--- packages/karton-contract/src/index.ts
+++ packages/karton-contract/src/index.ts
@@ -18,7 +18,23 @@ export const userMessageSchema = z.intersection(
 
 export type UserMessage = z.infer<typeof userMessageSchema>;
 
-export type History = (CoreAssistantMessage | CoreToolMessage | UserMessage)[];
+export type {
+  ToolResultPart,
+  ToolCallPart,
+  FilePart,
+  TextPart,
+  ImagePart,
+} from 'ai';
+
+export type ChatMessage = (
+  | CoreAssistantMessage
+  | CoreToolMessage
+  | UserMessage
+) & {
+  createdAt: Date;
+};
+
+export type History = ChatMessage[];
 type ChatId = string;
 export type Chat = {
   title: string;
@@ -35,6 +51,7 @@ type AgentError = {
 type AppState = {
   activeChatId: ChatId | null;
   chats: Record<ChatId, Chat>;
+  toolCallApprovalRequests: string[];
   isWorking: boolean;
 };
 
@@ -47,7 +64,10 @@ export type KartonContract = AppType<{
   serverProcedures: {
     createChat: () => Promise<string>;
     switchChat: (chatId: string) => Promise<void>;
+    deleteChat: (chatId: string) => Promise<void>;
     sendUserMessage: (message: UserMessage) => Promise<void>;
     abortAgentCall: () => Promise<void>;
+    approveToolCall: (toolCallId: string) => Promise<void>;
+    rejectToolCall: (toolCallId: string) => Promise<void>;
   };
 }>;
