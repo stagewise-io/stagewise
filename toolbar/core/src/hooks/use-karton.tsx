ee1a8fd603bc4df7caee744bb689110b0a184cff
Glenn Töws <glenntoews@outlook.com>
2025-08-10 09:44:09 -0700

    feat(toolbar): add karton

---
+++ toolbar/core/src/hooks/use-karton.tsx
@@ -0,0 +1,16 @@
+import type { KartonContract } from '@stagewise/karton-contract';
+import { createKartonReactClient } from '@stagewise/karton/react/client';
+
+const [KartonProvider, useKarton] = createKartonReactClient<KartonContract>({
+  webSocketPath: `${window.location.protocol}//${window.location.host}/stagewise-toolbar-app/karton/ws`,
+  procedures: {
+    getActiveChat: async () => 'hello',
+    getAvailableTools: async () => [],
+  },
+  fallbackState: {
+    chats: {},
+    isWorking: false,
+  },
+});
+
+export { KartonProvider, useKarton };
