3c8b9f0089d56e57bbe8c05e9c77492b86d4b239
Glenn Töws <glenntoews@outlook.com>
2025-08-10 10:16:45 -0700

    refactor(toolbar): update path for karton websocket connection

--- toolbar/core/src/hooks/use-karton.tsx
+++ toolbar/core/src/hooks/use-karton.tsx
@@ -2,7 +2,7 @@ import type { KartonContract } from '@stagewise/karton-contract';
 import { createKartonReactClient } from '@stagewise/karton/react/client';
 
 const [KartonProvider, useKarton] = createKartonReactClient<KartonContract>({
-  webSocketPath: `${window.location.protocol}//${window.location.host}/stagewise-toolbar-app/karton/ws`,
+  webSocketPath: `${window.location.protocol}//${window.location.host}/stagewise-toolbar-app/karton`,
   procedures: {
     getActiveChat: async () => 'hello',
     getAvailableTools: async () => [],
