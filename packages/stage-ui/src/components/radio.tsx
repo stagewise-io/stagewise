d21ea7cc655862f876df489610ac6cbdf457cbb8
Glenn Töws <glenntoews@outlook.com>
2025-10-27 17:30:19 +0100

    fix(stage-ui): fix sizing of radio group component

--- packages/stage-ui/src/components/radio.tsx
+++ packages/stage-ui/src/components/radio.tsx
@@ -10,7 +10,7 @@ export const RadioGroup = ({ className, ...props }: RadioGroupProps) => {
     <RadioGroupBase
       className={(state) =>
         cn(
-          'glass-inset flex size-5 shrink-0 flex-col items-start justify-start gap-2 rounded-full bg-white/20 disabled:opacity-50 data-[checked]:bg-primary',
+          'flex shrink-0 flex-col items-start justify-start gap-2 rounded-full bg-white/20 disabled:opacity-50 data-[checked]:bg-primary',
           typeof className === 'function' ? className(state) : className,
         )
       }
