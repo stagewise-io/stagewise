fe6a55195415abc0009f697fcac2fda278b23811
juliangoetze <90795588+JULO01@users.noreply.github.com>
2025-11-12 12:20:56 +0100

    feat(stage-ui): reduce size of checkbox component

--- packages/stage-ui/src/components/checkbox.tsx
+++ packages/stage-ui/src/components/checkbox.tsx
@@ -10,12 +10,12 @@ export function Checkbox(
     <BaseCheckbox.Root
       {...props}
       className={cn(
-        'glass-inset relative flex size-6 rounded-md p-1 transition-[background-position,box-shadow,background-color] duration-[150ms] ease-[cubic-bezier(0.26,0.75,0.38,0.45)] disabled:pointer-events-none disabled:opacity-50 data-[checked]:bg-blue-600',
+        'glass-inset relative flex size-5 rounded-md p-0.75 transition-[background-position,box-shadow,background-color] duration-[150ms] ease-[cubic-bezier(0.26,0.75,0.38,0.45)] disabled:pointer-events-none disabled:opacity-50 data-[checked]:bg-blue-600',
         props.className,
       )}
     >
-      <BaseCheckbox.Indicator className="translate-y-2 scale-25 duration-150 ease-out data-[checked]:translate-y-0 data-[checked]:scale-100">
-        <CheckIcon className="h-4 w-4 stroke-3 text-white" />
+      <BaseCheckbox.Indicator className="translate-y-1.5 scale-25 duration-150 ease-out data-[checked]:translate-y-0 data-[checked]:scale-100">
+        <CheckIcon className="h-3.5 w-3.5 stroke-3 text-white" />
       </BaseCheckbox.Indicator>
     </BaseCheckbox.Root>
   );
