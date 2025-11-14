a549def4c3496d0d895888578d5d11ee5a205cb4
Glenn Töws <glenntoews@outlook.com>
2025-11-14 21:35:34 +0100

    feat(cli): make chat scroll area wider

--- toolbar/core/src/screens/main/sections/sidebar/chat/_components/index.tsx
+++ toolbar/core/src/screens/main/sections/sidebar/chat/_components/index.tsx
@@ -74,7 +74,7 @@ export function ChatPanel({
       <ChatPanelHeader multiChatControls={multiChatControls} />
       <PanelContent
         className={cn(
-          'block px-0 py-0',
+          '-mx-3.5 block px-0 py-0',
           'h-full min-h-64',
           'mask-alpha mask-[linear-gradient(to_bottom,transparent_0px,black_48px,black)]',
           'overflow-hidden rounded-[inherit]',
