67987e9955eb98a71aedf0bfb2000ea76b236003
juliangoetze <90795588+JULO01@users.noreply.github.com>
2025-11-18 11:39:18 +0100

    feat(stage-ui): add dark mode toggle to storybook

--- packages/stage-ui/.storybook/main.ts
+++ packages/stage-ui/.storybook/main.ts
@@ -1,21 +1,18 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-import path, { join, dirname } from 'node:path';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')));
-}
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    getAbsolutePath('@storybook/addon-docs'),
-    getAbsolutePath('@storybook/addon-onboarding'),
+    '@storybook/addon-docs',
+    '@storybook/addon-onboarding',
+    '@vueless/storybook-dark-mode',
   ],
   framework: {
-    name: getAbsolutePath('@storybook/react-vite'),
+    name: '@storybook/react-vite',
     options: {},
   },
   async viteFinal(config) {
