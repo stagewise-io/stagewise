57c5d64080265e9baf4b1734498a3890d8c01d78
Glenn Töws <glenntoews@outlook.com>
2025-11-27 08:21:31 +0100

    chore: remove dependency on env-paths

--- apps/browser/src/backend/services/global-data-path.ts
+++ apps/browser/src/backend/services/global-data-path.ts
@@ -1,6 +1,4 @@
-import envPaths, { type Paths } from 'env-paths';
 import path from 'node:path';
-import { getEnvMode } from '@/utils/env';
 import fs from 'node:fs/promises';
 import type { Logger } from './logger';
 import { app } from 'electron';
@@ -9,16 +7,9 @@ import { app } from 'electron';
  * This service provides the paths to a variety of global data directories that this app can use to store data and configurations etc.
  */
 export class GlobalDataPathService {
-  private paths: Paths;
   private logger: Logger;
   private constructor(logger: Logger) {
     this.logger = logger;
-    this.paths = envPaths(
-      getEnvMode() === 'dev' ? 'stagewise-dev' : 'stagewise',
-      {
-        suffix: '',
-      },
-    );
   }
 
   public static async create(logger: Logger): Promise<GlobalDataPathService> {
@@ -30,10 +21,8 @@ export class GlobalDataPathService {
 
   private async initialize(): Promise<void> {
     // Create all paths that are needed (if they don't exist already)
-    await fs.mkdir(this.paths.config, { recursive: true });
-    await fs.mkdir(this.paths.data, { recursive: true });
-    await fs.mkdir(this.paths.cache, { recursive: true });
-    await fs.mkdir(this.paths.temp, { recursive: true });
+    await fs.mkdir(this.globalDataPath, { recursive: true });
+    await fs.mkdir(this.globalTempPath, { recursive: true });
   }
 
   get configFilePath(): string {
